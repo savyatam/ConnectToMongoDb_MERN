44312a501a2e20d0b2a1c5e38be72551002bda3c
savyatam <ysav1999@rediffmail.com>
2019-12-23 23:27:22 +0530

    3rd step

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app=express();
 const http=require('http').Server(app);
 const port=process.env.PORT||4000;
 const routes=require('./routes/api');
+const Bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URL,
+  {
+   useUnifiedTopology: true,
+   useNewUrlParser: true
+  }
+);
+mongoose.Promise = global.Promise;
+app.use(Bodyparser.json());
 app.get('/',function(req,res){
   res.send("It works");
 })
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,37 @@
 const express= require('express');
 const router=express.Router();
-router.get('/findpage',function(req,res)
+const Ninja=require('../models/ninjas');
+
+/*router.get('/findpage',function(req,res)
 {
   res.send("get works");
+});*/
+router.get("/found", (req, res, next) => {
+  Ninja.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      //   if (docs.length >= 0) {
+      res.status(200).json(docs);
+      //   } else {
+      //       res.status(404).json({
+      //           message: 'No entries found'
+      //       });
+      //   }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
+
 router.post('/findpage',function(req,res)
 {
-  res.send("post works");
+  Ninja.create(req.body).then(function(ninja){
+        res.send(ninja);
+      });
 });
 router.delete('/findpage/:id',function(req,res)
 {
